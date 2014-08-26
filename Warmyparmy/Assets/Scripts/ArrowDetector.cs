683c5a59846307f8f6e46ab6d10a8ebe2b3c1dd8
Chanisco <chaniscotromp@chaniscos-mbp.media.local>
2014-08-26 11:57:41 +0200

    ALL OVER AGAIN!

---
+++ Warmyparmy/Assets/Scripts/ArrowDetector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowDetector : MonoBehaviour {
+	bool boolLinks	= false;
+	bool boolRechts = false;
+	bool boolOmhoog	= false;
+	bool boolOmlaag	= false;
+	void Update(){
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.tag == Tags.PijltjeLinks){
+			boolLinks = true;
+		}
+		
+		if(other.tag == Tags.PijltjeRechts){
+			boolRechts = true;
+		}
+		
+		if(other.tag == Tags.PijltjeOmhoog){
+			boolOmhoog = true;
+		}
+		
+		if(other.tag == Tags.PijltjeLinks){
+			boolLinks = true;
+		}
+
+	}
+}
---
+++ Warmyparmy/Assets/Scripts/Tags.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Tags{
+	public static string PijltjeOmhoog 	= "PijltjeOmhoog";
+	public static string PijltjeOmlaag 	= "PijltjeOmlaag";
+	public static string PijltjeLinks 	= "PijltjeLinks";
+	public static string PijltjeRechts 	= "PijltjeRechts";
+}
