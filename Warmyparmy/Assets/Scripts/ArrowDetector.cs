0e65ea671dfd98372ab8846a3e7ccf4b47b97561
Swan Chase <16647@ma-web.nl>
2014-08-26 13:23:49 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Warmyparmy/Warmyparmy-csharp.sln
    	Warmyparmy/Warmyparmy.sln
    	Warmyparmy/Warmyparmy.userprefs

--- Warmyparmy/Assets/Scripts/ArrowDetector.cs
+++ Warmyparmy/Assets/Scripts/ArrowDetector.cs
@@ -2,30 +2,82 @@
 using System.Collections;
 
 public class ArrowDetector : MonoBehaviour {
-	bool boolLinks	= false;
-	bool boolRechts = false;
-	bool boolOmhoog	= false;
-	bool boolOmlaag	= false;
-	void Update(){
+	bool boolLeft	= false;
+	bool boolRight 	= false;
+	bool boolDown	= false;
+	bool boolUp		= false;
 
+	bool clear 		= true;
+
+	void Update(){
+		ButtonCheck();
 	}
 
 	void OnTriggerEnter(Collider other){
-		if(other.tag == Tags.PijltjeLinks){
-			boolLinks = true;
+		if(other.tag == Tags.arrowLeft){
+			clear = false;
+			boolLeft = true;
 		}
 		
-		if(other.tag == Tags.PijltjeRechts){
-			boolRechts = true;
+		if(other.tag == Tags.arrowRight){
+			clear = false;
+			boolRight = true;
 		}
 		
-		if(other.tag == Tags.PijltjeOmhoog){
-			boolOmhoog = true;
+		if(other.tag == Tags.arrowUp){
+			clear = false;
+			boolUp = true;
 		}
 		
-		if(other.tag == Tags.PijltjeLinks){
-			boolLinks = true;
+		if(other.tag == Tags.arrowDown){
+			clear = false;
+			boolDown = true;
 		}
 
 	}
+
+	void OnTriggerStay(Collider other){
+		if(clear && other.gameObject != null){
+			Destroy(other.gameObject);
+		}
+
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.tag == Tags.arrowLeft){
+			boolLeft = false;
+		}
+		
+		if(other.tag == Tags.arrowRight){
+			boolRight = false;
+		}
+		
+		if(other.tag == Tags.arrowUp){
+			boolUp = false;
+		}
+		
+		if(other.tag == Tags.arrowDown){
+			boolDown = false;
+		}
+	}
+
+	void ButtonCheck(){
+		if(Input.GetKeyDown(KeyCode.UpArrow) 	&& boolUp){
+			Debug.Log ("This is up");
+			clear = true;
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow) 	&& boolDown){
+			Debug.Log ("Ain't letting you down");
+			clear = true;
+		}
+		if(Input.GetKeyDown(KeyCode.LeftArrow) 	&& boolLeft){
+			Debug.Log ("Left is always right");
+			clear = true;
+		}
+		if(Input.GetKeyDown(KeyCode.RightArrow) && boolRight){
+			Debug.Log ("Never knew right over wrong...");
+			clear = true;
+		}
+	}
+	
 }
