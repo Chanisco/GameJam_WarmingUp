460e26943cb1ef6452188dc7153566f42d6c4573
yosha langius <jan@collie.nl>
2014-08-27 09:00:12 +0200

    arrowspawn 55%
    i need help

--- Warmyparmy/Assets/ArrowSpawn.cs
+++ Warmyparmy/Assets/ArrowSpawn.cs
@@ -4,19 +4,28 @@ using System.Collections;
 public class ArrowSpawn : MonoBehaviour {
 
 
-	public Rigidbody arrowholder;
+	public Rigidbody arrowone;
+	public Rigidbody arrowtwo;
+	public Rigidbody arrowthree;
+	public Rigidbody arrowfour;
+
+
 
 	
 	
 	void Update ()
 	{
+		if (Random.Range (0, 4) == 1) 
+		{
+			Instantiate(arrowthree);
+		}
 
 
 
 		if(Input.GetKeyDown(KeyCode.Space))
 		{
 
-			Instantiate(arrowholder);
+			Instantiate(arrowone);
 			Debug.Log("it works");
 
 		}
