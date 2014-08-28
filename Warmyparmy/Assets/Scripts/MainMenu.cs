eb4c0a2be5d1170453ca52b26752c5323426f5aa
yosha langius <jan@collie.nl>
2014-08-28 13:22:10 +0200

    sync

---
+++ Warmyparmy/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Texture backgroundTexture;
+
+	void onGUI()
+	{
+		//display background texture
+		GUI.DrawTexture (new Rect (0, 0, Screen.width,Screen.height), backgroundTexture);
+
+		//display buttons
+
+		if(GUI.Button(new Rect(Screen.width * .5f, Screen.height * .5f, Screen.width * .5f, Screen.height * .1f), "Play Game")){
+
+		}
+
+	}
+
+
+}
