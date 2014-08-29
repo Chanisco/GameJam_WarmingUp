de31c49b7dd4dd0daec54a0beb83518ac67c3157
Swan Chase <16647@ma-web.nl>
2014-08-29 11:59:34 +0200

    Score bar

---
+++ Warmyparmy/Assets/Scripts/Scorebar.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * 29 / 09 / 14  
+ * Swan Chase
+ * FDH
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class Scorebar : MonoBehaviour 
+{
+	public Texture2D backTexture;
+	public Texture2D BarTexture;
+	public Texture2D frontTexture;
+
+	public Vector2 Position = new Vector2(20,20);
+	public Vector2 Size = new Vector2(150,30);
+
+	private int Max ;
+
+	void Start ()
+	{
+		Max = Beats.Ritme.Count;
+	}
+
+	void OnGUI()
+	{
+		if (backTexture != null)
+		{
+			GUI.DrawTexture(new Rect(Position.x,Position.y,Size.x,Size.y),backTexture);
+		}
+		if (BarTexture != null)
+		{
+			int current = ArrowDetector.totalSuccessiveHits;
+			float percentage = (float)current / (float)Max;
+			GUI.DrawTexture(new Rect(Position.x,Position.y,Size.x * percentage,Size.y),BarTexture);
+		}
+		if (frontTexture != null)
+		{
+			GUI.DrawTexture(new Rect(Position.x,Position.y,Size.x,Size.y),frontTexture);
+		}
+	}
+}
